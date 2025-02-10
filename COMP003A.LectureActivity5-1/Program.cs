30e2305fcd3f3b06b3f1123831b4f17bf859b3ec
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-02-10 13:48:01 -0800

    Add project files.

---
+++ COMP003A.LectureActivity5-1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+// Author: Isaac Martinez
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Demonstrate the creation and usage of classes in C#
+*/
+namespace COMP003A.LectureActivity5_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
